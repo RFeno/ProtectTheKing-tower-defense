993547f18c70ba54617d5cf00eb97df80c2625ad
Adamaheto Saviour <skaytroz@gmail.com>
2022-11-11 23:06:07 +0100

    correction method start of wave

--- pTowerDefense/src/model/Game.cpp
+++ pTowerDefense/src/model/Game.cpp
@@ -16,14 +16,14 @@ Game::Game(string nameOfPlayer)
 {
     this->player = new Player(nameOfPlayer);
     this->king = new King();
-    this->mapOfGame = new Map();
+    //this->mapOfGame = new Map();
 }
 //dtor
 Game::~Game()
 {
     delete player;
     delete king;
-    delete mapOfGame;
+    //delete mapOfGame;
 }
 
 Game::Game(const Game& other)
@@ -40,46 +40,40 @@ Game& Game::operator=(const Game& rhs)
 /* launch the wave, generat*/
 void Game::startWave(int numberOfEnemies)
 {
+    KnightOfDeath knightofd;
+    Orc orc;
+    Ogre ogre;
+    ShadowMonster shadowM;
+
     for(int i = 1; i<= numberOfEnemies;i++)
     {
         int typeOfEnemy = rand()%4 +1;
 
         //creation of ennemies randomly
+
         switch(typeOfEnemy)
         {
-
             case 1:
                 {
-                    cout << to_string(mapOfGame->getEnemies().size()) << endl;
-                    //ask if we need to delete if we clone
-                    KnightOfDeath* knightofd = new KnightOfDeath;
-                    mapOfGame->getEnemies().push_back(knightofd->clone());
-
-                    //delete knightofd;
+                    //mapOfGame.getEnemies().push_back(knightofd.clone());
+                    mapOfGame->getEnemies().push_back(knightofd.clone());
                     break;
                 }
             case 2:
                 {
-                    cout << to_string(mapOfGame->getEnemies().size()) << endl;
-                    Ogre* ogre = new Ogre;
-                    this->mapOfGame->getEnemies().push_back(ogre->clone());
-                    delete ogre;
+
+
+                    mapOfGame->getEnemies().push_back(ogre.clone());
                     break;
                 }
             case 3:
                 {
-                    cout << to_string(mapOfGame->getEnemies().size()) << endl;
-                    Orc* orc = new Orc;
-                    mapOfGame->getEnemies().push_back(orc->clone());
-                    delete orc;
+                    mapOfGame->getEnemies().push_back(orc.clone());
                     break;
                 }
             case 4:
                 {
-                    cout << to_string(mapOfGame->getEnemies().size()) << endl;
-                    //ShadowMonster* shadowM = ;
-                    mapOfGame->getEnemies().push_back(new ShadowMonster);
-                    //delete shadowM;
+                    mapOfGame->getEnemies().push_back(shadowM.clone());
                     break;
                 }
 
