d8e5e4252bada7ea4b51b5e35b83a061621e1711
Adamaheto Saviour <skaytroz@gmail.com>
2022-11-14 20:12:52 +0100

    add sprites management with differents type of enemy

--- pTowerDefense/include/view/vGameBoard.h
+++ pTowerDefense/include/view/vGameBoard.h
@@ -11,43 +11,27 @@ class vGameBoard
 {
     public:
 
-        const int WALK_SPEED = 1;
+        const int WALK_SPEED = 2;
+        int distanceMove =1;
 
-        vGameBoard();
+        vGameBoard(RenderWindow& window);
         virtual ~vGameBoard();
         vGameBoard(const vGameBoard& other);
         vGameBoard& operator=(const vGameBoard& other);
 
-
-
-        Texture mapTexture;
-        Texture pauseTexture;
-        //Texture musiqueOnTexture;
-        Texture acideCloudTexture;
-        Texture lightningTexture;
-        Texture fireTexture;
-        std::vector<Sprite> enemiesSprite;
-        Texture enemyTexture;
-
-        Sprite mapSprite;
-        Sprite pauseSprite;
-        //Sprite musiqueOnSprite;
-        Sprite acideCloudSprite;
-        Sprite lightningSprite;
-        Sprite fireSprite;
-        Sprite enemySprite;
-
-        void launchView(RenderWindow& window);
+        void launchView();
         void loadSprite();
         void loadFont();
         bool verifyImage();
-        void drawEntities(RenderWindow& window);
-        void InputHandler(Event event, RenderWindow &window);
+        bool drawEntities(RenderWindow& window);
+        void InputHandler(Event event, RenderWindow *window);
         bool isSpriteClicked (Sprite &spr, RenderWindow &window);
 
         //methods
         void launchGame();
+        void launchWave(int numberOfEnemies);
         void animationEnemyWalk();
+        void animationEnemyWalk2(Sprite *enemy);
         void enemiesSpawn();
         void displayEnd();
         bool setTower(int y, int x, int type);
@@ -56,11 +40,72 @@ class vGameBoard
         void activeLightningSpeel();
         void activeCloudSpell();
 
+
+
+
+
     protected:
 
     private:
+        bool animatedEnemmyWalk;
         Game game;
-        std::vector<Sprite*> listOfEnemies;
+        RenderWindow *windowFromMain;
+
+        const int OGRE_WIDTH = 377 ;
+        const int OGRE_HEIGHT = 404;
+        const int ORC_WIDTH = 339;
+        const int ORC_HEIGHT = 353 ;
+        const int GREMLIN_WIDTH = 238;
+        const int GREMLIN_HEIGHT = 227;
+        const int SHADOWMONSTER_WIDTH = 326;
+        const int SHADOWMONSTER_HEIGHT = 302;
+        const int KNIGHTOFDEATH_WIDTH = 445;
+        const int KNIGHTOFDEATH_HEIGHT = 469;
+
+        //x and y serve to reset the animation of sprites in fonction of diffrents size of sprite
+        int x_Ogre = 0;
+        int y_Ogre = 0;
+
+        int x_Orc = 0;
+        int y_Orc = 0;
+
+        int x_gremlin = 0;
+        int y_gremlin = 0;
+
+        int x_shadowMonster = 0;
+        int y_shadowMonster = 0;
+
+        int x_knight = 0;
+        int y_knight = 0;
+
+        Clock animClock;
+        Clock spawnClock;
+
+
+        //view
+        Texture mapTexture;
+        Texture pauseTexture;
+        Texture acideCloudTexture;
+        Texture lightningTexture;
+        Texture fireTexture;
+        std::vector<Sprite*> enemiesSprite;
+
+        Texture gremlinTexture;
+        Texture knightOfDeathTexture;
+        Texture shadowMonsterTexture;
+        Texture ogreTexture;
+        Texture orcTexture;
+
+        Sprite mapSprite;
+        Sprite pauseSprite;
+        //Sprite musiqueOnSprite;
+        Sprite acideCloudSprite;
+        Sprite lightningSprite;
+        Sprite fireSprite;
+
+
+
+
 };
 
 #endif // VGAMEBOARD_H
